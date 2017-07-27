a05f25faad696122915e98b47c02204ad426caad
N3evin <n3evin@hotmail.com>
2017-07-27 18:03:57 +1000

    Set amiiboJsonEncoder into app.

--- app.py
+++ app.py
@@ -5,11 +5,13 @@
 @license: MIT License
 """
 
+import datetime
+from flask.json import JSONEncoder
+from Amiibo.filterable import FilterableCollection
 from flask import Flask, jsonify, abort, make_response, render_template, request
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from Amiibo.manager import AmiiboManager
-from Amiibo.amiiboJsonEncoder import AmiiboJSONEncoder
 from Amiibo.amiibo import (
     Hex,
     AmiiboHex,
@@ -19,8 +21,48 @@ from Amiibo.amiibo import (
     AmiiboTypeHex,
     AmiiboModelHex,
     AmiiboSeriesHex,
+    Amiibo,
+    AmiiboReleaseDates,
+    GameSeries,
+    Character,
+    AmiiboType,
+    AmiiboSeries,
 )
 
+class AmiiboJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Hex):
+            return str(obj)
+        elif isinstance(obj, FilterableCollection):
+            return list(obj)
+        elif isinstance(obj, Amiibo):
+            return {
+                'name': obj.name,
+                'head': str(obj.head)[2:],
+                'tail': str(obj.tail)[2:],
+                'type': obj.amiibo_type.name if obj.amiibo_type else None,
+                'gameSeries': obj.game_series.name if obj.game_series else None,
+                'amiiboSeries': obj.amiibo_series.name if obj.amiibo_series else None,
+                'character': obj.character.name if obj.character else None,
+                'image': "https://raw.githubusercontent.com/N3evin/AmiiboAPI/master/images/icon_{}-{}.png".format(str(obj.head)[2:], str(obj.tail)[2:]),
+                'release': obj.release,
+            }
+        elif isinstance(obj, (GameSeries, Character, AmiiboType, AmiiboSeries)):
+            return {
+                'key': obj.id,
+                'name': obj.name,
+            }
+        elif isinstance(obj, AmiiboReleaseDates):
+            return {
+                'na': obj.na,
+                'jp': obj.jp,
+                'eu': obj.eu,
+                'au': obj.au,
+            }
+        elif isinstance(obj, (datetime.date, datetime.datetime, datetime.time)):
+            return obj.isoformat()
+
+        return super().default(obj)
 
 app = Flask(__name__)
 app.json_encoder = AmiiboJSONEncoder
